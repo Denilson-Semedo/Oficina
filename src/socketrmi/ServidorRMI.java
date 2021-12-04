d6a7ba783a2f160261dc1e494549215677d5097a
Denilson Semedo <dstavares00@gmail.com>
2021-12-04 11:45:21 -0100

    Criando camada de dados >> Consultar peças; >> Entrada de veiculo; >> Saida de veiculo;

--- src/socketrmi/ServidorRMI.java
+++ src/socketrmi/ServidorRMI.java
@@ -14,6 +14,9 @@ import java.rmi.Naming;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  *
@@ -21,12 +24,20 @@ import java.sql.PreparedStatement;
  */
 public class ServidorRMI {
 
-    public static void entrada_veiculo(String matricula, String dono, String data, String problema) {
+    private static String getDate() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public static void entrada_veiculo(String matricula, String dono, String problema) {
 
         //Apanhar as credencias de acesso a base de dados nas variaveis de ambiente;
         String db_user = System.getenv("BD_USER");
         String db_pass = System.getenv("BD_PASS");
-
+        
+        String data = getDate();
+        
         try {
             // create a mysql database connection
             String myUrl = "jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=UTC";
@@ -34,7 +45,7 @@ public class ServidorRMI {
             Connection connect = DriverManager.getConnection(myUrl, db_user, db_pass);
 
             // the mysql insert statement
-            String query = "insert into entrada_veiculoID (matricula,dono,data_entrada,problema) values (?,?,?,?)";
+            String query = "insert into entrada_veiculo (matricula,dono,data_entrada,problema) values (?,?,?,?)";
 
             // create the mysql insert preparedstatement
             PreparedStatement preparedStmt = connect.prepareStatement(query);
@@ -55,7 +66,7 @@ public class ServidorRMI {
         }
     }
 
-    public static String consultar_pecas(int codigo) {
+    public static String consultar_peca(int codigo) {
 
         //Apanhar as credencias de acesso a base de dados nas variaveis de ambiente;
         String db_user = System.getenv("BD_USER");
@@ -89,12 +100,82 @@ public class ServidorRMI {
         return pecaQtd;
     }
 
+    public static void saida_veiculo(int entrada_veiculoID, String solucao) {
+
+        //Apanhar as credencias de acesso a base de dados nas variaveis de ambiente;
+        String db_user = System.getenv("BD_USER");
+        String db_pass = System.getenv("BD_PASS");
+
+        String data = getDate();
+        
+        try {
+            // create a mysql database connection
+            String myUrl = "jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=UTC";
+            //Class.forName(myDriver);
+            Connection connect = DriverManager.getConnection(myUrl, db_user, db_pass);
+
+            // the mysql insert statement
+            String query = "insert into saida_veiculo (entrada_veiculoID,data_saida,solucao) values (?,?,?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = connect.prepareStatement(query);
+            preparedStmt.setInt(1, entrada_veiculoID);
+            preparedStmt.setString(2, data);
+            preparedStmt.setString(3, solucao);
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            connect.close();
+            String resposta = "\nSaída de veiculo Feita com sucesso!!";
+            System.err.println(resposta);
+
+        } catch (Exception e) {
+            System.err.println("\nGot an exception!" + e.getMessage());
+        }
+
+    }
+    
+    public static String consultar_pecasALL(){
+        
+        //Apanhar as credencias de acesso a base de dados nas variaveis de ambiente;
+        String db_user = System.getenv("BD_USER");
+        String db_pass = System.getenv("BD_PASS");
+
+        String pecaQtd = "";
+
+        try {
+            // create a mysql database connection
+            String myUrl = "jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=UTC";
+            //Class.forName(myDriver);
+            Connection connect = DriverManager.getConnection(myUrl, db_user, db_pass);
+
+            // the mysql select statement
+            String sql = "SELECT * FROM pecas;";
+            PreparedStatement pstm = connect.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+
+            while (rs.next()) {
+                pecaQtd = rs.getString("quantidade");
+            }
+            rs.close();
+            pstm.close();
+
+            connect.close();
+
+        } catch (Exception e) {
+            System.err.println("\nGot an exception!" + e.getMessage());
+        }
+        
+        return "";
+    }
+
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         LocateRegistry.createRegistry(1099);
         System.setProperty("java.rmi.server.hostname", "localhost");
         //Naming.rebind("rmi://localhost:1099/testeHello", new ImplHello());
         Naming.rebind("testeHello", new ImplHello());
         System.out.println("Serviço disponivel....");
-
+        
     }
 }
